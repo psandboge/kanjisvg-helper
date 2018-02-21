6e7a3369a0eb96391a5aa68b201168f8226cdf57
Pierre Sandboge <pierre@sandboge.com>
2018-02-21 15:27:07 +0100

    Work in progress

---
+++ src/main/java/se/sandboge/kanjisvghelper/Convert.java
@@ -0,0 +1,52 @@
+package se.sandboge.kanjisvghelper;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.io.StringWriter;
+
+public class Convert {
+    public static void main(String[] args) {
+        String sourcePath = "/hej.xml";
+        String template = "/template.xsl";
+
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        try {
+            factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(Convert.class.getResourceAsStream(sourcePath));
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            StreamSource ss = new StreamSource(Convert.class.getResourceAsStream(template));
+            Transformer transformer = transformerFactory.newTransformer(ss);
+
+            DOMSource domSource = new DOMSource(document);
+            StringWriter stringWriter = new StringWriter();
+            StreamResult result = new StreamResult(stringWriter);
+            transformer.transform(domSource, result);
+            System.out.println(stringWriter.toString());
+
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/resource/template.xsl
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<xsl:stylesheet
+        xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+        version="1.0"
+>
+    <xsl:strip-space elements="*"/>
+    <xsl:output method="text"/>
+
+    <xsl:template match="svg">
+            <xsl:apply-templates/>
+    </xsl:template>
+
+    <xsl:template match="g">
+            <xsl:apply-templates/>
+    </xsl:template>
+
+    <xsl:template match="path">
+        <xsl:value-of select="attribute::d"/>
+        <xsl:text>&#xa;</xsl:text>
+    </xsl:template>
+
+    <xsl:template match="text">
+        <xsl:value-of select="attribute::transform"/>
+        <xsl:text>,</xsl:text>
+            <xsl:apply-templates/>
+        <xsl:text>&#xa;</xsl:text>
+    </xsl:template>
+</xsl:stylesheet>
